3c5b5793f8b2ede163c2681db016576441fd94d7
antor <antormondal157@gmail.com>
2026-03-06 22:16:04 +0530

    Add initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # First-App
-This is my first App....
+<h1>This is my first App </h1>
+<h3>You are welcome to this repository</h3>
---
+++ test.py
@@ -0,0 +1,6 @@
+#  Addition of two number
+
+x =int(input("Enter first number:"))
+y =int(input("Enter secound number;"))
+z =x+y
+print('sum of the number=',z)
